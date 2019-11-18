47c94caed4c184b823f2477aabdb742117a42e3e
kunle babatunde <kb2232@nyu.edu>
2019-11-18 05:36:07 -0500

    first commit

---
+++ notes.md
@@ -0,0 +1,7 @@
+# important key concepts
+  ## reuseability
+    1. a component can be used the same everywhere within the app
+  ## nesting
+    1. components can be nested
+  ## configurations
+    1. the settings do not change
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css'
+
+const App=()=>{
+  return(
+    <div className="component">
+      HELLO PEOPLEs
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
